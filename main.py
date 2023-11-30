8da67f4a1256d743ae16fa56e63273c0ade92bc9
Cedric Kouame <kouamekoffi20@outlook.fr>
2023-11-30 21:55:16 +0000

    final

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class DbIntegrator:
         self.config_vars = json.load(self.CONF_FILE)
         self.xml_path = self.config_vars['XMLPATH']
         self.output = self.config_vars['OUTPUT']
+        self.output_kcp = self.config_vars['OUTPUTKCP']
         self.old_bases = self.config_vars['OLDBASES']
         self.bases = self.config_vars['BASES']
         self.categories = self.config_vars['CATEGORIES']
@@ -23,6 +24,8 @@ class DbIntegrator:
         # CREATION DES REPERTOIRES D'ENTREE ET DE SORTIE DES XML
         if not os.path.exists(self.output):
             os.makedirs(self.output)
+        if not os.path.exists(self.output_kcp):
+            os.makedirs(self.output_kcp)
         if not os.path.exists(self.xml_path):
             os.makedirs(self.xml_path)
 
@@ -36,6 +39,8 @@ class DbIntegrator:
             # tree = ET.parse('files/Client.xml')
             for file in files:
                 n = 0
+                category_list_array = []
+                string_list_array = []
                 base_from_filename = file.split('.xml')[0].upper()
                 if base_from_filename in base:
                     file_with_fullpath = self.xml_path + '/' + file
@@ -44,20 +49,27 @@ class DbIntegrator:
                         tree = ET.parse(file_with_fullpath)
                         root = tree.getroot()
                         tagIndex = root.findall(".//Object/Tag/TagIndex")
+                        document_root = root.findall(".//Object/document/page/uri")
+                        document_array = [doc.text for doc in document_root]
+
                         # category = root.findall(".//Object/Tag/TagIndex/Category")
-                        print(len(tagIndex))
+
                         # value = [val.text for val in root.findall(".//Value/string")]
                         for tag in tagIndex:
 
                             category = tag.findall(".//Category")
                             value = tag.findall(".//Value/string")
-
+                            category_array = []
+                            string_array = []
+                            for string in value:
+                                string_array.append(string.text)
                             for child in category:
                                 number = child.find(".//number")
                                 child.attrib['Name'] = self.categories[base][child.attrib['Name']]
                                 number.text = self.bases['AUTRES'].get(child.attrib['Name'])
-                                category_array: array.array.append([child.attrib['Name']])
-                            print(category_array)
+                                category_array.append(child.attrib['Name'])
+                            category_list_array.append(category_array)
+                            string_list_array.append(string_array)
 
                         output_file = self.output + '/' + file
                         tree.write(output_file, encoding="UTF-8", xml_declaration=True)
@@ -73,9 +85,7 @@ class DbIntegrator:
                         f = open(output_file, 'w')
                         f.writelines(final_content)
                         f.close()
-                        val_array = [val.text for val in root.findall(".//Value/string")]
-                        name_array = [cat.attrib['Name'] for cat in root.findall(".//Object/Tag/TagIndex/Category")]
-                        self.create_xml(tagIndex, value, val_array, name_array, file)
+                        self.create_xml(category_list_array, string_list_array, document_array, file)
 
                         logging.info('Traitement terminé')
                 else:
@@ -84,29 +94,33 @@ class DbIntegrator:
             logging.exception(e)
             return False
 
-    def create_xml(self, datas, base, value, name, file):
-        if datas:
+    def create_xml(self, cat_array, string_array, doc_array, file):
+        if cat_array:
             n = 0
             root = minidom.Document()
             xml = root.createElement('root')
             root.appendChild(xml)
-            for i in range(0, len(datas)):
+            for i in range(0, len(cat_array)):
 
                 documentChild = root.createElement('document')
                 xml.appendChild(documentChild)
-                nb = len(base)
+                nb = len(cat_array[i])
+                field = root.createElement('field')
+                field.setAttribute('name', 'Document Filename with Full Path')
+                field.setAttribute('value', doc_array[i])
+                documentChild.appendChild(field)
                 for j in range(0, nb):
                     field = root.createElement('field')
-                    field.setAttribute('name', name[j])
-                    field.setAttribute('value', value[n])
+                    field.setAttribute('name', cat_array[i][j])
+                    field.setAttribute('value', string_array[i][j])
                     documentChild.appendChild(field)
-                    n = n + 1
 
             """if not os.path.exists("XML"):
                 os.mkdir("XML")"""
             xml_str = root.toprettyxml(indent="\t")
-            if not os.path.exists(file):
-                f = open(file, "w")
+            output_kcp_file = self.output_kcp+'/'+file
+            if not os.path.exists(output_kcp_file):
+                f = open(output_kcp_file, "w")
             f.write(xml_str)
             f.close()
         else:
